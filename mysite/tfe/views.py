8401099f348b1fded082f8a80549a60c19f259d7
Overial <dfjx2201@yandex.ru>
2021-11-01 21:20:11 +0300

    Added unauthenticated user redirection (pages restriction)

--- mysite/tfe/views.py
+++ mysite/tfe/views.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 from .models import Tab
@@ -16,29 +17,29 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tfe:tabs')
 
 
-class TabList(ListView):
+class TabList(LoginRequiredMixin, ListView):
     model = Tab
     context_object_name = 'tabs'
 
 
-class TabDetail(DetailView):
+class TabDetail(LoginRequiredMixin, DetailView):
     model = Tab
     context_object_name = 'tab'
 
 
-class TabCreate(CreateView):
+class TabCreate(LoginRequiredMixin, CreateView):
     model = Tab
     fields = '__all__'
     success_url = reverse_lazy('tfe:tabs')
 
 
-class TabUpdate(UpdateView):
+class TabUpdate(LoginRequiredMixin, UpdateView):
     model = Tab
     fields = '__all__'
     success_url = reverse_lazy('tfe:tabs')
 
 
-class TabDelete(DeleteView):
+class TabDelete(LoginRequiredMixin, DeleteView):
     model = Tab
     context_object_name = 'tab'
     success_url = reverse_lazy('tfe:tabs')
