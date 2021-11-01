0628fb839ef69c764be67e9d760aac1254b2879d
Overial <dfjx2201@yandex.ru>
2021-11-01 13:59:05 +0300

    [TFE] Created and migrated Tab model

--- mysite/tfe/models.py
+++ mysite/tfe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tab(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    descr = models.TextField(null=True, blank=True)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
