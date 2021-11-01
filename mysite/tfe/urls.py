b4ffb3fd81b6e4a446b90c9da41918d43d2ec9d2
Overial <dfjx2201@yandex.ru>
2021-11-01 22:11:54 +0300

    Added user creation; Added searching functionality

--- mysite/tfe/urls.py
+++ mysite/tfe/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
-from .views import TabList, TabDetail, TabCreate, TabUpdate, TabDelete, CustomLoginView
+from .views import TabList, TabDetail, TabCreate, TabUpdate, TabDelete, CustomLoginView, RegisterPageView
 
 app_name = 'tfe'
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='tfe:login'), name='logout'),
+    path('register/', RegisterPageView.as_view(), name='register'),
     path('', TabList.as_view(), name='tabs'),
     path('tab/<int:pk>/', TabDetail.as_view(), name='tab'),
     path('tab_create/', TabCreate.as_view(), name='tab_create'),
--- mysite/tfe/views.py
+++ mysite/tfe/views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 
 from .models import Tab
@@ -17,19 +20,52 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tfe:tabs')
 
 
+class RegisterPageView(FormView):
+    template_name = 'tfe/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tfe:tabs')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPageView, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tfe:tabs')
+        return super(RegisterPageView, self).get(*args, **kwargs)
+
+
 class TabList(LoginRequiredMixin, ListView):
     model = Tab
     context_object_name = 'tabs'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_input = self.request.GET.get('search_area') or ''
+
+        if search_input:
+            context['tabs'] = context['tabs'].filter(
+                title__icontains=search_input
+                # title__startswith=search_input
+            )
+
+        context['search_input'] = search_input
+
+        return context
+
 
 class TabDetail(LoginRequiredMixin, DetailView):
     model = Tab
+    fields = '__all__'
     context_object_name = 'tab'
 
 
 class TabCreate(LoginRequiredMixin, CreateView):
     model = Tab
-    fields = '__all__'
+    fields = ['band', 'album', 'title', 'instrument', 'description']
     success_url = reverse_lazy('tfe:tabs')
 
 
