a1da40cae618e6fd2c29b4e7588a35071467e06e
Overial <dfjx2201@yandex.ru>
2021-11-01 21:12:13 +0300

    Added login/logout functionality

--- mysite/tfe/urls.py
+++ mysite/tfe/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
-from .views import TabList, TabDetail, TabCreate, TabUpdate
+from .views import TabList, TabDetail, TabCreate, TabUpdate, TabDelete, CustomLoginView
 
 app_name = 'tfe'
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='tfe:login'), name='logout'),
     path('', TabList.as_view(), name='tabs'),
     path('tab/<int:pk>/', TabDetail.as_view(), name='tab'),
     path('tab_create/', TabCreate.as_view(), name='tab_create'),
     path('tab_update/<int:pk>', TabUpdate.as_view(), name='tab_update'),
+    path('tab_delete/<int:pk>', TabDelete.as_view(), name='tab_delete'),
 ]
--- mysite/tfe/views.py
+++ mysite/tfe/views.py
@@ -1,9 +1,19 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
 from .models import Tab
+
+
+class CustomLoginView(LoginView):
+    template_name = 'tfe/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tfe:tabs')
 
 
 class TabList(ListView):
@@ -25,4 +35,10 @@ class TabCreate(CreateView):
 class TabUpdate(UpdateView):
     model = Tab
     fields = '__all__'
+    success_url = reverse_lazy('tfe:tabs')
+
+
+class TabDelete(DeleteView):
+    model = Tab
+    context_object_name = 'tab'
     success_url = reverse_lazy('tfe:tabs')
